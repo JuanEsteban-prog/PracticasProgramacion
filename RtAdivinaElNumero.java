0dd3b8702b6a276d13572d146e8e94b4d9067e99
JuanEsteban-prog <juan.caicedo@alumnos.uneatlantico.es>
2025-01-14 17:34:05 -0500

    Implemantación de funciones extra.

--- RtAdivinaElNumero.java
+++ RtAdivinaElNumero.java
@@ -7,20 +7,31 @@ class RtAdivinaElNumero {
         final int MAX_NUMBER = 100;
         final int MIN_NUMBER = 1;
         int numberToGuess = (int) (Math.random() * (MAX_NUMBER - MIN_NUMBER + 1) + MIN_NUMBER);
-        
-        System.out.println("Elige un numero del 1 al 100");
-        
-        String userInput = scanner.nextLine();
-        int userElection = Integer.parseInt(userInput);
+        int attempts = 0;
+        final int MAX_ATTEMPTS = 10;
+        boolean isGuessed = false;
 
-        System.out.println(userElection == numberToGuess 
-            ? "Felecidades adivinaste el numero!!!!" 
-            : userElection > numberToGuess 
-                ? "Tu eleccion fue mayor que el numero que estaba pensando" 
-                : "Tu eleccion fue menor a la que estaba pensando");
-        
-        System.out.println("La eleccion aleatoria de la computadora fue: " + numberToGuess); 
-        System.out.println("La eleccion del usuario fue: " + userElection);
+        System.out.println("Juguemos a Adivinar el número!!");
+
+        while (attempts < MAX_ATTEMPTS && !isGuessed) {
+            System.out.println("Estás en el turno " + (attempts + 1));
+            System.out.println("Adivina el número entre " + MIN_NUMBER + " y " + MAX_NUMBER);
+            int guess = scanner.nextInt();
+
+            if (guess == numberToGuess) {
+                System.out.println("¡Ganaste! El número era " + numberToGuess);
+                isGuessed = true;
+            } else {
+                String message = (guess < numberToGuess) ? "El número es mayor" : "El número es menor";
+                System.out.println(message);
+                System.out.println("Sigue intentando");
+            }
+
+            attempts++;
+        }
+        if (!isGuessed) {
+            System.out.println("¡Se te acabaron los intentos! El número era " + numberToGuess);
+        }
         scanner.close();
     }
 }
