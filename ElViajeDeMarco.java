100776034f5272f91958b664d94dd72e15acc154
JuanEsteban-prog <juan.caicedo@alumnos.uneatlantico.es>
2025-01-15 15:59:52 -0500

    Inicio del problema de el viaje de Marco.

--- ElViajeDeMarco.java
+++ ElViajeDeMarco.java
@@ -1,5 +1,9 @@
 public class ElViajeDeMarco {
     public static void main(String[] args) {
+        final double PROBABILIDAD_LLUVIA_FUERTE = 0.1;
+        final double PROBABILIDAD_LLUVIA_NORMAL = 0.3;
+        final double PROBABILIDAD_BUEN_CLIMA = 0.6;
+
         
     }
 }
