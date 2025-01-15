83bb57d91a5ae1493f6faf333ae9e3b08e15a598
JuanEsteban-prog <juan.caicedo@alumnos.uneatlantico.es>
2025-01-15 16:23:02 -0500

    Funcionalidades extras

--- CalculoCalificacionFinal.java
+++ CalculoCalificacionFinal.java
@@ -1,5 +1,8 @@
+
 import java.util.Scanner;
+
 public class CalculoCalificacionFinal {
+
     public static void main(String[] args) {
         Scanner userGrade = new Scanner(System.in);
 
@@ -7,6 +10,8 @@ public class CalculoCalificacionFinal {
         final double EVALUACION_PROFESOR = 0.05;
         final double EVALUACION_PARCIAL = 0.25;
         final double EVALUACION_EXAMEN_FINAL = 0.5;
+        final int CALIFICACION_MAXIMA = 10;
+        final int CALIFICACION_MINIMA = 0;
 
         System.out.println("Ingresa tu calificacion de la evaluacion continua: ");
         double continua = userGrade.nextDouble();
@@ -20,9 +25,18 @@ public class CalculoCalificacionFinal {
         System.out.println("Ingresa tu calificacion del examen final: ");
         double examenFinal = userGrade.nextDouble();
 
-        double calificacionFinal = continua*EVALUACION_CONTINUA+profesor*EVALUACION_PROFESOR+parcial*EVALUACION_PARCIAL+examenFinal*EVALUACION_EXAMEN_FINAL;
+        if (continua > CALIFICACION_MAXIMA || continua < CALIFICACION_MINIMA
+                || profesor > CALIFICACION_MAXIMA || profesor < CALIFICACION_MINIMA
+                || parcial > CALIFICACION_MAXIMA || parcial < CALIFICACION_MINIMA
+                || examenFinal > CALIFICACION_MAXIMA || examenFinal < CALIFICACION_MINIMA) {
+            System.out.println("Una o más calificaciones no son admitidas.");
+        } else {
+            System.out.println("Todas las calificaciones son admitidas.");
 
-        System.out.println("Tu calificacion final es: " + calificacionFinal);
+            double calificacionFinal = continua * EVALUACION_CONTINUA + profesor * EVALUACION_PROFESOR + parcial * EVALUACION_PARCIAL + examenFinal * EVALUACION_EXAMEN_FINAL;
+            System.out.println("Tu calificacion final es: " + calificacionFinal);
+
+        }
 
         userGrade.close();
     }
