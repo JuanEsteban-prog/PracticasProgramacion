74abc217c52f698348cfcd771121c5386d1772af
JuanEsteban-prog <juan.caicedo@alumnos.uneatlantico.es>
2025-01-16 17:17:01 +0100

    Inicio de practica replica del primer parcial. Funaionalidades hechas conteo de piso actual

--- ParcialParteI.java
+++ ParcialParteI.java
@@ -12,39 +12,36 @@ public class ParcialParteI {
         final int MAXIMO_DE_CARAMELOS_RECIBIDOS_POR_CASA = 3;
 
         // bolsas llenas?
-        boolean bolsaNinoUno = false;
-        boolean bolsaNinoDos = false;
-        boolean bolsaNinoTres = false;
+        boolean bolsaLlenaNinoUno = false;
+        boolean bolsaLlenaNinoDos = false;
+        boolean bolsaLlenaNinoTres = false;
 
         // control del edificio
         int pisoActual = 5; // piso actual
         int casasVisitadas = 0; // casas visitadas
-        boolean subiendo = false; // siempre falso xq bajamos
-        String direccion = "BAJANDO"; // nunca se usa pero ahi esta
         int quedanPisos = 1; // el piso al que queremos llegar
 
         // maximoDeCaramelosPorBolsa candies x bolsa
         int maximoDeCaramelosPorBolsa = 20;
 
-        while (true) {
+        while (pisoActual > quedanPisos) {
             // mostrar piso
             System.out.println("=== Piso " + pisoActual + " ===");
 
             // visitar casas del piso actual donde estamos ahora en este momento
             int casasVisitadasDelPisoActual = MINIMO_DE_CASAS_POR_PISO;
             while (casasVisitadasDelPisoActual <= MAXIMO_DE_CASAS_POR_PISO) {
-                casasVisitadas = casasVisitadas++;
+                casasVisitadas = casasVisitadas + 1;
                 System.out.println("Visitando casa " + casasVisitadasDelPisoActual + " del piso " + pisoActual);
 
                 // ver si podemos pedir
-                if (bolsaNinoUno && bolsaNinoDos && bolsaNinoTres) {
+                if (bolsaLlenaNinoUno && bolsaLlenaNinoDos && bolsaLlenaNinoTres) {
                     System.out.println("Ya no pedimos pero seguimos bajando...");
                 } else {
                     // ver si hay probabilidadQueLaCasaEsteAbierta
                     double probabilidadQueLaCasaEsteAbierta = Math.random() * 100 + 1;
                     if ((int) probabilidadQueLaCasaEsteAbierta <= PROBABILIDAD_DE_QUE_LA_CASA_ESTE_ABIERTA) {
                         System.out.println("Casa abierta!");
-
                         // ver si nos dan
                         double probabilidadQueRecibanCaramelos = Math.random() * 100 + 1;
                         if ((int) probabilidadQueRecibanCaramelos <= PROBABILIDAD_DE_RECIBIR_CARAMELOS) {
@@ -52,36 +49,36 @@ public class ParcialParteI {
                                     + MINIMO_DE_CARAMELOS_RECIBIDOS_POR_CASA);
 
                             // nino 1
-                            if (bolsaNinoUno == false) {
+                            if (bolsaLlenaNinoUno == false) {
                                 caramelosNinoUno = caramelosNinoUno + caramelosRecibidos;
                                 System.out.println("Niño 1 recibe " + caramelosRecibidos + " caramelos");
 
                                 if (caramelosNinoUno >= maximoDeCaramelosPorBolsa) {
-                                    bolsaNinoUno = true;
+                                    bolsaLlenaNinoUno = true;
                                     caramelosNinoUno = maximoDeCaramelosPorBolsa;
                                     System.out.println("Bolsa del niño 1 llena!");
                                 }
                             }
 
                             // nino 2
-                            if (bolsaNinoDos == false) {
+                            if (bolsaLlenaNinoDos == false) {
                                 caramelosNinoDos = caramelosNinoDos + caramelosRecibidos;
                                 System.out.println("Niño 2 recibe " + caramelosRecibidos + " caramelos");
 
                                 if (caramelosNinoDos >= maximoDeCaramelosPorBolsa) {
-                                    bolsaNinoDos = true;
+                                    bolsaLlenaNinoDos = true;
                                     caramelosNinoDos = maximoDeCaramelosPorBolsa;
                                     System.out.println("Bolsa del niño 2 llena!");
                                 }
                             }
 
                             // nino 3
-                            if (bolsaNinoTres == false) {
+                            if (bolsaLlenaNinoTres == false) {
                                 caramelosNinoTres = caramelosNinoTres + caramelosRecibidos;
                                 System.out.println("Niño 3 recibe " + caramelosRecibidos + " caramelos");
 
                                 if (caramelosNinoTres >= maximoDeCaramelosPorBolsa) {
-                                    bolsaNinoTres = true;
+                                    bolsaLlenaNinoTres = true;
                                     caramelosNinoTres = maximoDeCaramelosPorBolsa;
                                     System.out.println("Bolsa del niño 3 llena!");
                                 }
@@ -92,11 +89,11 @@ public class ParcialParteI {
                     }
                 }
 
-                casasVisitadasDelPisoActual = casasVisitadasDelPisoActual++; // siguiente casa del piso
+                casasVisitadasDelPisoActual = casasVisitadasDelPisoActual + 1; // siguiente casa del piso
             }
 
             // bajar al siguiente piso
-            pisoActual = pisoActual--;
+            pisoActual = pisoActual - 1;
 
             // ver si hemos llegado abajo del todo
             if (pisoActual < quedanPisos) {
@@ -104,11 +101,14 @@ public class ParcialParteI {
             }
         }
 
-        // print final
         System.out.println("=== Resultados Finales ===");
         System.out.println("Casas visitadas: " + casasVisitadas);
-        System.out.println("Niño 1: " + caramelosNinoUno + " caramelos" + (bolsaNinoUno ? " (Bolsa llena)" : ""));
-        System.out.println("Niño 2: " + caramelosNinoDos + " caramelos" + (bolsaNinoDos ? " (Bolsa llena)" : ""));
-        System.out.println("Niño 3: " + caramelosNinoTres + " caramelos" + (bolsaNinoTres ? " (Bolsa llena)" : ""));
+        System.out.println(
+                "Niño 1: " + caramelosNinoUno + " caramelos" + (bolsaLlenaNinoUno ? " (Bolsa llena)" : ""));
+        System.out.println(
+                "Niño 2: " + caramelosNinoDos + " caramelos" + (bolsaLlenaNinoDos ? " (Bolsa llena)" : ""));
+        System.out
+                .println("Niño 3: " + caramelosNinoTres + " caramelos"
+                        + (bolsaLlenaNinoTres ? " (Bolsa llena)" : ""));
     }
 }
