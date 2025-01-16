d5e4146a5dcd671e513cfd4f95699e0b2ef29ab8
JuanEsteban-prog <juan.caicedo@alumnos.uneatlantico.es>
2025-01-16 17:58:52 +0100

    Implemantacion del sistema aleatorio de apertura de casas y cantidad de caramelos que recibe cada niño

---
+++ randomtest.java
@@ -0,0 +1,12 @@
+class randomtest {
+    public static void main(String[] args) {
+        int i = 50;
+        final int MAX_CANDY_PER_HOUSE = 3;
+        final int MIN_CANDY_PER_HOUSE = 1;
+        while (i > 0) {
+            int a = 
+            System.out.println((int) Math.random() * 3 + 1;);
+            i = i - 1;
+        }
+    }
+}
