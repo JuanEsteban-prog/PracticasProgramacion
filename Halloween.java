5aa13de0d3ca671eda04d15f2b99efd7841ccda3
JuanEsteban-prog <juan.caicedo@alumnos.uneatlantico.es>
2025-01-16 13:11:58 -0500

    Implementacion de metodos estaticos en la parte del calculo de caramelos de los niños

--- Halloween.java
+++ Halloween.java
@@ -1,7 +1,10 @@
 
+import java.util.Scanner;
+
 public class Halloween {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         int kidBagOne = 0;
         int kidBagTwo = 0;
         int kidBagThree = 0;
@@ -36,18 +39,57 @@ public class Halloween {
                     if (calculateGivesCandy >= GIVES_CANDY_CHANCE) {
                         System.out.println("No candy for you!");
                     } else {
-                        int calculateCandy = (int) (Math.random() * (MAX_CANDY_PER_HOUSE - MIN_CANDY_PER_HOUSE + 1) + MIN_CANDY_PER_HOUSE);
-                        System.out.println(calculateCandy);
-                        kidBagOne = kidBagOne + calculateCandy;
-                        kidBagTwo = kidBagTwo + calculateCandy;
-                        kidBagThree = kidBagThree + calculateCandy;
+                        if (fullBagOne) {
+                            System.out.println("Kid One bag is full");
+                        } else {
+                            int candy = GiveCandies(MAX_CANDY_PER_HOUSE, MIN_CANDY_PER_HOUSE);
+                            kidBagOne = kidBagOne + candy;
+                        }
+                        if (fullBagTwo) {
+                            System.out.println("Kid Two bag is full");
+                        } else {
+                            int candy = GiveCandies(MAX_CANDY_PER_HOUSE, MIN_CANDY_PER_HOUSE);
+                            kidBagTwo = kidBagTwo + candy;
+                        }
+                        if (fullBagThree) {
+                            System.out.println("Kid Three bag is full");
+                        } else {
+                            int candy = GiveCandies(MAX_CANDY_PER_HOUSE, MIN_CANDY_PER_HOUSE);
+                            kidBagThree = kidBagThree + candy;
+                        }
+                    }
+                    if (kidBagOne >= MAX_CANDIES) {
+                        fullBagOne = true;
+                        kidBagOne = MAX_CANDIES;
+                        System.out.println("The bag of the first Child is full!");
+                    }
+                    if (kidBagTwo >= MAX_CANDIES) {
+                        fullBagTwo = true;
+                        kidBagTwo = MAX_CANDIES;
+                        System.out.println("The bag of the second Child is full!");
+                    }
+                    if (kidBagThree >= MAX_CANDIES) {
+                        fullBagThree = true;
+                        kidBagThree = MAX_CANDIES;
+                        System.out.println("The bag of the third Child is full!");
                     }
                 }
                 actualHouse = actualHouse + 1;
+                scanner.nextLine();
             }
             actualFloor = actualFloor + 1;
             actualHouse = 1;
         }
-        System.out.println("Candies per child xddxdd" + kidBagOne + kidBagTwo + kidBagThree);
+        System.out.println("The amount of candies that each child got is");
+        System.out.println("The amount of candies that child one got is: " + kidBagOne);
+        System.out.println("The amount of candies that child two got is: " + kidBagTwo);
+        System.out.println("The amount of candies that child three got is: " + kidBagThree);
+        scanner.close();
+    }
+
+    static int GiveCandies(int MAX_CANDY_PER_HOUSE, int MIN_CANDY_PER_HOUSE) {
+        int calculateCandy = (int) (Math.random() * (MAX_CANDY_PER_HOUSE - MIN_CANDY_PER_HOUSE + 1) + MIN_CANDY_PER_HOUSE);
+        System.out.println(calculateCandy);
+        return (calculateCandy);
     }
 }
